8f035cf764377b168c7c29bc68750b2ace0c8f4b
SylasB <sylasboniek@gmail.com>
2022-11-03 11:49:39 -0600

    Added some javascript to test.

---
+++ script.js
@@ -0,0 +1,14 @@
+let library = [];
+
+function book(title, author, pages, read) {
+}
+
+function addBookToLibrary() {
+    var inp = document.getElementById('author').value;
+    library.push(inp);
+    console.log(library);
+    return false;
+}
+
+//const story = new addBookToLibrary('Tolkein', 'hobbit', '295', 'yes');
+
