1237d820351fd4bb18a851a10db774a4304b6063
SylasB <sylasboniek@gmail.com>
2022-11-03 15:19:01 -0600

    Added skeletal JavaScript to add new books to the DOM.

--- script.js
+++ script.js
@@ -3,12 +3,41 @@ let library = [];
 function book(title, author, pages, read) {
 }
 
+const inputBtn = document.getElementById("authorBtn");
+const myText = document.getElementById("authorText");
+const results = document.getElementById("results");
+
+inputBtn.addEventListener("click", e => {
+const titleName = document.createElement('div');
+   titleName.classList.add('titleName');
+   titleName.textContent =  'Title'
+
+   results.appendChild(titleName);
+
+const authorName = document.createElement('div');
+   authorName.classList.add('authorName');
+   authorName.textContent = `Author`;
+
+   titleName.appendChild(authorName);
+
+const pagesNum = document.createElement('div');
+    pagesNum.classList.add('pagesNum');
+    pagesNum.textContent = '#';
+
+    titleName.appendChild(pagesNum);
+
+const readYet = document.createElement('div');
+    readYet.classList.add('readYet');
+    readYet.textContent = 'Yes/No';
+
+    titleName.appendChild(readYet);
+})
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+
 function addBookToLibrary() {
-    var inp = document.getElementById('author').value;
-    library.push(inp);
-    console.log(library);
-    return false;
+    
 }
 
-//const story = new addBookToLibrary('Tolkein', 'hobbit', '295', 'yes');
-
+//const story = new addBookToLibrary('Tolkein', 'hobbit', '295', 'yes'); 
